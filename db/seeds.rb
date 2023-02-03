9fd448bc0f9b3e621ef918f26305e8ff6f64522d
Alexis Parave <aparavev@gmail.com>
2023-02-03 14:29:59 -0500

    :sparkles: Add seeds for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Begin seeds'
+puts 'Seeding Products'
+
+pro1 = Product.create(sku: 'SKU-1001', name: 'Product 1', description: 'This is a description', price: 1000, stock: 10)
+pro2 = Product.create(sku: 'SKU-1002', name: 'Product 2', description: 'This is a description', price: 2000, stock: 20)
+pro3 = Product.create(sku: 'SKU-1003', name: 'Product 3', description: 'This is a description', price: 5000, stock: 50)
+
+puts 'End seeds'
